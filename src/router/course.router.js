aef2392665e3c4860f94ca3059639939fde54e26
Majra <majra.oruc@gmail.com>
2021-05-06 20:27:06 +0200

    CRUD

--- src/router/course.router.js
+++ src/router/course.router.js
@@ -4,6 +4,12 @@ import ctrl from '../controllers/courses.controller';
 const router = express.Router();
 
 router.route('/courses')
-    .get(ctrl.list);
+    .get(ctrl.list)
+    .post(ctrl.create);
 
+router.route('/courses/:id')
+    .get(ctrl.read)
+    .put(ctrl.update)
+    .delete(ctrl.remove)
+    
 export default router;
--- src/router/student.router.js
+++ src/router/student.router.js
@@ -4,6 +4,12 @@ import ctrl from '../controllers/students.controller';
 const router = express.Router();
 
 router.route('/students')
-    .get(ctrl.list);
+    .get(ctrl.list)
+    .post(ctrl.create)
+
+router.route('/students/:id')
+    .get(ctrl.read)
+    .put(ctrl.update)
+    .delete(ctrl.remove)
 
 export default router;
